5e0e72d84c3ace4abfced193a97b2c8bdef8c22f
moliveira <moliveira@pinterest.com>
2024-04-04 04:13:47 -0700

    feat: :sparkles: Produce the message and waits for the response

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("mensagem", "mytest", producer, nil)
+	Publish("mensagem", "mytest", producer, nil, deliveryChan)
+
+	// wait for delivery report
+	e := <-deliveryChan
+	msg := e.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("Delivery failed: %v\n", msg.TopicPartition.Error)
+	} else {
+		fmt.Println("Message sent: ", msg.TopicPartition)
+	}
+
 	producer.Flush(1000)
 }
 
@@ -24,13 +36,13 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
